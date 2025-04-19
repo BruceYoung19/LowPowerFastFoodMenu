df2bb6127b9edee7a015def433e6a64d2c7f76b0
Bruce.young19 <Bruce7645@gmail.com>
2025-04-20 11:41:14 +1200

    <creation> [Database]:description
    	- Database skeleton added

--- database.py
+++ database.py
@@ -1,5 +1,31 @@
 import sqlite3
 
 def connectDatabase():
-    
+   with sqlite3.connect("foodshop.db") as connection:
+        
+       cusor = connection.cursor():
+       print ("Connected To The Database Successfully")
+       
+       # Making each of the database
 
+       create_menu_table = '''
+       '''
+
+       create_user_table= '''
+       '''
+       
+       create_order_table'''
+       '''
+       
+       cusor.execute(create_menu_table)
+       cusor.execute(create_user_table)
+       cusor.execute(create_order_table)
+       
+       connection.commit()
+
+def creatingusers():
+     with sqlite3.connect("foodshop.db") as connection:
+        cusor = connection.cursor();
+
+
+connectDatabase()
